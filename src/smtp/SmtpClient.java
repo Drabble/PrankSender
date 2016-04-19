76a0783289838e4a6e3574acbae13f58a2bc76d2
Guillaume Serneels <guillaume.serneels@heig-vd.ch>
2016-04-19 09:50:28 +0200

    Fix MacOSX CRLF

--- src/smtp/SmtpClient.java
+++ src/smtp/SmtpClient.java
@@ -16,10 +16,10 @@ import java.util.List;
  * SMTP client for sending emails
  */
 public class SmtpClient {
-    PrintWriter out;                  // Socket writer
+    DataOutputStream out;                  // Socket writer
     BufferedReader in;                // Socket reader
     private String smtpServerAddress; // SMTP server address
-    private int smtpServerPort = 25;  // SMTP server port
+    private int smtpServerPort;  // SMTP server port
     private Socket socket;            // Socket for the connection to the SMTP server
 
     /**
@@ -42,11 +42,13 @@ public class SmtpClient {
     public void sendMessages(List<Message> messages) throws IOException {
         // Create and connect the socket
         socket = new Socket(smtpServerAddress, smtpServerPort);
-        out = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), "UTF-8"), true);
+        out = new DataOutputStream(socket.getOutputStream());
         in = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
 
         // Write EHLO command
-        out.println("EHLO pranker");
+        //System.out.println(in.readLine());
+        out.writeBytes("EHLO pranker" + "\r\n");
+        //System.out.println("EHLO pranker");
         String s;
         do {
             s = in.readLine();
@@ -55,31 +57,31 @@ public class SmtpClient {
 
         for (Message message : messages) {
             // Set mail sender
-            out.println("MAIL FROM: " + message.getFrom());
+            out.writeBytes("MAIL FROM: " + message.getFrom() + "\r\n");
             System.out.println(in.readLine());
 
             // Set recipients
             for (String recipient : message.getTo()) {
-                out.println("RCPT TO: " + recipient);
+                out.writeBytes("RCPT TO: " + recipient + "\r\n");
                 System.out.println(in.readLine());
             }
 
             // Set email content
-            out.println("DATA");
+            out.writeBytes("DATA" + "\r\n");
             System.out.println(in.readLine());
-            out.println("From: " + message.getFrom());
-            out.print("To: ");
+            out.writeBytes("From: " + message.getFrom() + "\r\n");
+            out.writeBytes("To: ");
             for (String recipient : message.getTo()) {
-                out.print(recipient + ",");
+                out.writeBytes(recipient + "," );
             }
-            out.print("\n");
-            out.print(message.getContent());
-            out.println(".");
+            out.writeBytes("\r\n");
+            out.writeBytes(message.getContent()+ "\r\n");
+            out.writeBytes("." + "\r\n");
             System.out.println(in.readLine());
         }
 
         // Quit smtp server
-        out.println("quit");
+        out.writeBytes("quit"+ "\r\n");
         System.out.println(in.readLine());
 
         // Close socket
